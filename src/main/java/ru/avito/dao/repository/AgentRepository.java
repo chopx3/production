b47c2a7271a0d275b29f7612d7745cc88210fd19
chopX3 <idemenev.mj@gmail.com>
2017-04-26 16:26:48 +0300

    First?

--- src/main/java/ru/avito/dao/repository/AgentRepository.java
+++ src/main/java/ru/avito/dao/repository/AgentRepository.java
@@ -1,9 +1,8 @@
 package ru.avito.dao.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import ru.avito.model.agent.Agent;
+
 
 /**
  * Created by Dmitriy on 26.02.2017.
