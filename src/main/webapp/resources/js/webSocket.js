d576397ec742b092fc4cecd2f5c8b754eee26b9a
chopX3 <idemenev.mj@gmail.com>
2017-05-20 12:45:28 +0300

    comments5

--- src/main/webapp/resources/js/webSocket.js
+++ src/main/webapp/resources/js/webSocket.js
@@ -3,17 +3,13 @@ var webSocketHost = location.host+'/firecatcher';
 var websocket;
 var websocketUrl = "ws://"+webSocketHost+"/websocket/start";
 var wsCount = 0;
-
-
 startConnection();
-
 function startConnection() {
     websocket = new WebSocket(websocketUrl);
         websocket.onopen = function () {
             online();
         }
 }
-
 function sendWebSocketMessage(message){
     if(websocket.readyState === 1){
     websocket.send(message);
@@ -25,16 +21,13 @@ function sendWebSocketMessage(message){
         }, 1000);
     }
 }
-
 websocket.onerror = function (err) {
     setWebSocketStatus("Error");
     console.log(err);
 };
-
 websocket.onclose = function () {
     setWebSocketStatus("Offline");
 };
-
 function getWebsocketMessage(callback){
     websocket.onmessage = function (webSocketMessage) {
         switch (webSocketMessage.data){
@@ -59,7 +52,6 @@ function online() {
 		$("#websocketStatus").text("Online");
             console.log("ok")
 		}
-
 function setWebSocketStatus(status) {
     document.getElementById("websocketStatus").innerHTML = status;
     document.getElementById("websocketStatus").style.color = "red";
