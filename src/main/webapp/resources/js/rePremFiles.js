a550c9de28c11a1339d74e6d46509378d25d0c17
chopX3 <idemenev.mj@gmail.com>
2017-09-19 22:02:48 +0300

    ip + reprem

--- src/main/webapp/resources/js/rePremFiles.js
+++ src/main/webapp/resources/js/rePremFiles.js
@@ -1,36 +1,57 @@
-var clientsFromBIBaseMap = new Map();
 var clientsInFCBaseMap = new Map();
 var clientsToAddFromBI = new Map();
 var clientsFromBIBaseArray = [];
 var clientsInFCBaseArray = [];
-var result = [];
 var finalCSVFile = [];
-var headerNames = ["External_id", "name", "ADMIN Contact Name", "Phone from admin", "телефон1"];
-var neededHeaders = ["avito_id", "username", "contact_person", "adm_phone", "contact_phone"];
-var arrayOfObjectsOfActiveUsers = [];
+var correctNumbers = [];
 var wrongNumbers = []; 
 $(document).ready(function() {
-    $('#files').change(function(){ readFile(); });
+    $('#files').change(function(){ start(); });
 });
-function readFile() {
-    var files = document.getElementById('files').files;
-    var file = files[0];
-    var ourData = "";
-    var reader = new FileReader();
-    reader.onloadend = function(evt) {
-      if (evt.target.readyState == FileReader.DONE) { // DONE == 2
-        ourData = evt.target.result;
+function start() {
+  var files = document.getElementById('files').files;
+  var file = files[0];
+  readFile(file)
+  .then(function(csv) { return map=convertCsvToMap(csv); })
+  .then(function(map){ return updatedMap = updateMap(map) })
+  .then(function(updatedMap){ 
+    setTimeout(function(){  
+    for (var [key, value] of updatedMap) {
+      if (value["inBase"] == false && key>0){ 
+        checkNumber(value);
+        addClient(value);
       }
     };
-    reader.readAsText(file);
-    setTimeout(function(){csvJSON(ourData);
-    getAllRepremUsers();
-    setTimeout(function(){checkClientsToAdd(clientsInFCBaseMap, result);}, 1500);
-    createCSV();    }, 2000);
-    console.log(clientsInFCBaseMap);
-    console.log(clientsFromBIBaseMap);
-  }
-function csvJSON(csv){
+    var date = new Date();
+    downloadCSV({
+              data: dedupe(correctNumbers),
+              filename: "reprem-"+date.getHours()+"-"+date.getMinutes()+".csv"
+          })
+    downloadCSV({
+              data: dedupe(wrongNumbers),
+              filename: "wrongNumbers-"+date.getHours()+"-"+date.getMinutes()+".csv"
+          })
+  }, 1000)
+  });
+}
+function readFile(file){
+  return new Promise(function(resolve, reject) {
+  var reader = new FileReader();
+  reader.readAsText(file);
+  reader.onloadend = function(evt) {
+    if (evt.target.readyState == FileReader.DONE) { // DONE == 2
+      resolve(evt.target.result);
+    }
+  };
+})
+}
+function convertCsvToMap(csv){
+  var headerNames = ["External_id", "name", "ADMIN Contact Name", "Phone from admin", "телефон1"];
+  var neededHeaders = ["avitoId", "username", "contactPerson", "admPhone", "contactPhone"];
+  var clientsFromBIBaseMap = new Map();
+  var regExpTab = /\t/gm;
+  csv = csv.replace(regExpTab, ";");
+  console.log(csv);
   var lines=csv.split("\n");
   console.log(lines);
   var mapResult = [];
@@ -39,104 +60,90 @@ function csvJSON(csv){
   for(var i=1;i<lines.length;i++){
     var obj = {};
     var currentline=lines[i].split(";");
+    var combinedArrayLine = [];
+    var lineID = 0;
     for(var j=0;j<headers.length;j++){
       if (headerNames.indexOf(headers[j])+1){
-      obj[neededHeaders[headerNames.indexOf(headers[j])]] = currentline[j];
+      combinedArrayLine[neededHeaders[headerNames.indexOf(headers[j])]] = currentline[j];
       }
-      if (neededHeaders[headerNames.indexOf(headers[j])] === "avito_id"){
-        clientsFromBIBaseMap.set(+currentline[j], "true");
+      if (neededHeaders[headerNames.indexOf(headers[j])] === "avitoId"){
+        lineID = currentline[j];
       }
     }
-    result.push(obj);
+    combinedArrayLine["inBase"] = false;
+    clientsFromBIBaseMap.set(+lineID, combinedArrayLine);
   }
-  console.log(result);
+  return clientsFromBIBaseMap;
 }
-function setActiveStatus(status, id){
-    var client ={
-                "avitoId" : id,
-                "active"  : +status
-              };
-              RestPost(client, updateRepremActiveURL);
-}
-function getAllRepremUsers(){
-$.get(getAllRepremUsersURL).done(function (data) {
-  var info = data;
-  for (var i = 0;i<info.length;i++){
-    if(clientsFromBIBaseMap.has(info[i].avitoId) != info[i].active){setActiveStatus(clientsFromBIBaseMap.has(info[i].avitoId), info[i].avitoId);}
-      clientsInFCBaseMap.set(info[i].avitoId, true);
+function updateMap(map) {
+  var updatedMap = map;
+  $.get(getAllRepremUsersURL).done(function (data) {
+  var info = data;  
+  for (var i = 0;i<info.length;i++) {
+    if ( updatedMap.has(info[i].avitoId) && info[i].avitoId > 0 ) {
+    checkNumber(info[i]);
+    var idArray = updatedMap.get(info[i].avitoId);
+    idArray["inBase"] = true;
+    updatedMap.set(info[i].avitoId, idArray);
   }
-  }) 
+  }
+})
+return updatedMap;
 }
-function checkClientsToAdd(map, array){
-    console.log(map);
-    console.log(array);
-    for (var i=0; i<array.length; i++){
-        if(!map.has(+array[i].avito_id) && +array[i].avito_id > 0){
-            var newClient = {
-                "avitoId" : array[i].avito_id,
-                "admPhone" : array[i].adm_phone,
-                "contactPerson" :  array[i].contact_person,
-                "contactPhone" :  array[i].contact_phone,
-                "username" :  array[i].username,
-                "active" : 1
-            }
-            RestPost(newClient, addRepremURL);
-        }
-    }
+function addClient(client) {
+  var newClient = {
+      "avitoId" : client.avitoId,
+      "admPhone" : client.admPhone,
+      "contactPerson" :  client.contactPerson,
+      "contactPhone" :  client.contactPhone,
+      "username" :  client.username,
+      "active" : 1
+  }
+  RestPost(newClient, addRepremURL);
 }
-function createCSV(){
-$.get(getAllRepremUsersURL).done(function (data) {
-  var info = data;
+function checkNumber(number){  
   var regExpMultilines = /(\r\n|\n|\r)\1{1,}/gm;
   var phoneRegExp = /([7|8])([0-9]{10})/gm;
-  for (var i = 0;i<info.length;i++){
-    var obj = {};
-    if (info[i].active){
-        var nameWOComma = (info[i].username).replace(",", ".");
-        var phone = (info[i].admPhone+"").replace(phoneRegExp, "$2");
-            obj = {
-                "Name" : info[i].avitoId + ", " + nameWOComma,
-                "Phone" : +phone            
-            }
-            if (phone<1000000000 || phone>70000000000){wrongNumbers.push(obj);}
-            else {arrayOfObjectsOfActiveUsers.push(obj)};
-            if (info[i].contactPhone > 0){
-                var phone = (info[i].contactPhone+"").replace(phoneRegExp, "$2");
-                obj = {
-                "Name" : info[i].avitoId + ", " + nameWOComma,
-                "Phone" : +phone            
-                }
-                if (phone<1000000000 || phone>70000000000){wrongNumbers.push(obj);}
-            else {arrayOfObjectsOfActiveUsers.push(obj)};  
-            }
-            if(info[i].additionalPhones !== null){
-                if ((info[i].additionalPhones).replace(regExpMultilines, "") !== null && (info[i].additionalPhones).replace(regExpMultilines, "") !== "" && (info[i].additionalPhones).replace(regExpMultilines, "") !== " "){
-                    var numbers = (info[i].additionalPhones).replace(regExpMultilines, "\n").trim();
-                    var numbersArray = numbers.split("\n");
-                    for (var j=0;j<numbersArray.length; j++){
-                        var phone = parseInt((numbersArray[j]+"").replace(phoneRegExp, "$2"));
-                        obj = {
-                        "Name" : info[i].avitoId + ", " + nameWOComma,
-                        "Phone" : phone           
-                        }
-                        if (phone<1000000000 || phone>70000000000 || phone === NaN){wrongNumbers.push(obj);}
-                        else {arrayOfObjectsOfActiveUsers.push(obj)};  
-                    }
-                }       
-            }           
-    }   
-  } 
-  var date = new Date();
-  downloadCSV({
-            data: dedupe(arrayOfObjectsOfActiveUsers),
-            filename: "reprem-"+date.getHours()+"-"+date.getMinutes()+".csv"
-        })
-  downloadCSV({
-            data: dedupe(wrongNumbers),
-            filename: "wrongNumbers-"+date.getHours()+"-"+date.getMinutes()+".csv"
-        })
-  })
+  var obj = {};
+  var username = number.username || "-";
+  var nameWOComma = username.replace(",", ".");
+  var phone = (number.admPhone+"").replace(phoneRegExp, "$2");
+      obj = {
+          "Name" : number.avitoId + ", " + nameWOComma,
+          "Phone" : +phone            
+      }
+      if (checkPhone(phone)){wrongNumbers.push(obj);}
+      else {correctNumbers.push(obj)};
+      if (number.contactPhone > 0){
+          var phone = (number.contactPhone+"").replace(phoneRegExp, "$2");
+          obj = {
+          "Name" : number.avitoId + ", " + nameWOComma,
+          "Phone" : +phone            
+          }
+          if (checkPhone(phone)){wrongNumbers.push(obj);}
+      else {correctNumbers.push(obj)};  
+      }
+      if(number.additionalPhones !== null && number.additionalPhones !== undefined) {
+          if ((number.additionalPhones).replace(regExpMultilines, "") !== null && (number.additionalPhones).replace(regExpMultilines, "") !== "" && (number.additionalPhones).replace(regExpMultilines, "") !== " "){
+              var numbers = (number.additionalPhones).replace(regExpMultilines, "\n").trim();
+              var numbersArray = numbers.split("\n");
+              for (var j=0;j<numbersArray.length; j++) {
+                  var phone = parseInt((numbersArray[j]+"").replace(phoneRegExp, "$2"));
+                  obj = {
+                  "Name" : number.avitoId + ", " + nameWOComma,
+                  "Phone" : phone           
+                  }
+                  if (checkPhone(phone)){wrongNumbers.push(obj);}
+                  else {correctNumbers.push(obj)};  
+              }
+          }       
+      }           
 }
+function checkPhone(phone){
+  var minNumber = 1000000000;
+  var maxNumber = 70000000000;
+  return (phone<minNumber || phone>maxNumber || phone === NaN) ;
+} 
 function dedupe(arr) {
   return arr.reduce(function (p, c) {
     var id = [c.Name, c.Phone].join('|');
@@ -148,42 +155,42 @@ function dedupe(arr) {
   }, { temp: [], out: [] }).out;
 }
 function convertArrayOfObjectsToCSV(args) {
-        var result, ctr, keys, columnDelimiter, lineDelimiter, data;
-        data = args.data || null;
-        if (data == null || !data.length) {
-            return null;
-        }
-        columnDelimiter = args.columnDelimiter || ';';
-        lineDelimiter = args.lineDelimiter || '\n';
-        keys = Object.keys(data[0]);
-        result = '';
-        result += keys.join(columnDelimiter);
-        result += lineDelimiter;
-        data.forEach(function(item) {
-            ctr = 0;
-            keys.forEach(function(key) {
-                if (ctr > 0) result += columnDelimiter;
+  var result, ctr, keys, columnDelimiter, lineDelimiter, data;
+  data = args.data || null;
+  if (data == null || !data.length) {
+      return null;
+  }
+  columnDelimiter = args.columnDelimiter || ';';
+  lineDelimiter = args.lineDelimiter || '\n';
+  keys = Object.keys(data[0]);
+  result = '';
+  result += keys.join(columnDelimiter);
+  result += lineDelimiter;
+  data.forEach(function(item) {
+      ctr = 0;
+      keys.forEach(function(key) {
+          if (ctr > 0) result += columnDelimiter;
 
-                result += item[key];
-                ctr++;
-            });
-            result += lineDelimiter;
-        });
-        return result;
-    }
+          result += item[key];
+          ctr++;
+      });
+      result += lineDelimiter;
+  });
+  return result;
+}
 function downloadCSV(args) {
-        var data, filename, link;
-        var csv = convertArrayOfObjectsToCSV({
-            data: args.data
-        });
-        if (csv == null) return;
-        filename = args.filename || 'export.csv';
-        if (!csv.match(/^data:text\/csv/i)) {
-            csv = 'data:text/csv;charset=cp1251,' + csv;
-        }
-        data = encodeURI(csv);
-        link = document.createElement('a');
-        link.setAttribute('href', data);
-        link.setAttribute('download', filename);
-        link.click();
+  var data, filename, link;
+  var csv = convertArrayOfObjectsToCSV({
+      data: args.data
+  });
+  if (csv == null) return;
+  filename = args.filename || 'export.csv';
+  if (!csv.match(/^data:text\/csv/i)) {
+      csv = 'data:text/csv;charset=cp1251,' + csv;
+  }
+  data = encodeURI(csv);
+  link = document.createElement('a');
+  link.setAttribute('href', data);
+  link.setAttribute('download', filename);
+  link.click();
 }
